4c6fee5dad002e70e550c823c9c593519b54a258
sriharsha1897 <sriharshaithal97@gmail.com>
2022-01-26 17:50:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,10 @@ import Chart2 from './components/Chart2';
 import Table from './components/Table';
 import TodoList from './components/TodoList';
 import UserDevice from './components/UsersDevice';
-// import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-// import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-  //   <div>
-  //     <Navbar/>
-  //   <Router>
-  //   <Switch>
-  //   <div className='todo-app'>
-  //     <Route exact path="/"><TodoList/></Route> 
-  //   </div>
-  //     <Route exact path="/chart1"><Chart1/></Route> 
-  //     <Route exact path="/chart1"><Chart1/></Route> 
-  //     <Route exact path="/table"><Table/></Route> 
-  //   </Switch>
-  //   </Router>
-  // </div>
     <div className='todo-app'>
       <TodoList/>
       <UserDevice />
@@ -34,4 +20,3 @@ function App() {
 }
 
 export default App;
-// "both": "concurrently \" npm run start\" \"nodemon backend/index.js\""
